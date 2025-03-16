86f6c5c8dd659d8b6793770ac1393dd5b3847b63
Tran Nguyen Quoc Bao <122146748+tnqbao@users.noreply.github.com>
2025-03-16 23:25:46 +0700

    update cicd workflow, dockerfile

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM golang:1.23-alpine AS builder
+WORKDIR /gau_phim
+COPY go.mod go.sum ./
+RUN go mod download
+COPY . .
+RUN go build -tags 'prod' -o main .
+
+FROM alpine:latest
+WORKDIR /gau_phim
+COPY --from=builder /gau_blog/main .
+EXPOSE 8083
+CMD ["./main"]
--- main.dev.go
+++ main.dev.go
@@ -1,1 +1,28 @@
+//go:build dev
+
 package main
+
+import (
+	"github.com/tnqbao/gau_blog_service/api/vote"
+	"github.com/tnqbao/gau_blog_service/lib/ai"
+	"log"
+
+	"github.com/joho/godotenv"
+	"github.com/tnqbao/gau_blog_service/config"
+	"github.com/tnqbao/gau_blog_service/routes"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	//config.InitRedis()
+	db := config.InitDB()
+
+	ai.LoadAPIKeys()
+	router := routes.SetupRouter(db)
+	go vote.StartSyncJob(db)
+	router.Run(":8085")
+}
