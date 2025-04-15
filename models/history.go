0b34bb09ce7b93fe87f5e2d5a6782db70cc17a1d
Tran Nguyen Quoc Bao <122146748+tnqbao@users.noreply.github.com>
2025-04-16 00:21:31 +0700

    fix error column in history

--- models/history.go
+++ models/history.go
@@ -4,7 +4,7 @@ type History struct {
 	ID           uint   `json:"id" gorm:"primary_key" `
 	UserId       uint   `json:"-"`
 	MovieName    string `json:"title"`
-	MovieSlug    string `json:"slug" gorm:"index;unique"`
+	MovieSlug    string `json:"slug" gorm:"index"`
 	MoviePoster  string `json:"poster_url"`
 	MovieEpisode string `json:"movie_episode"`
 	CreateAt     string `json:"create_at"`
